d5775ba3ce13655611b941a17a3bb3533c3dcb58
Pratap Bollina <PratapB@citiustech.com>
2022-08-30 11:56:46 +0530

    main added

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
-provider "aws" {
-access_key = var.access_key
-secret_key = var.secret_key
-region = var.region
+locals {
+  lambda_zip_location = outputs/stopec2
+}
+data "archive_file" "stopec2" {
+  type        = "zip"
+  source_file = "stopec2.py"
+  output_path = "${locals.lambda_zip_location}"
+}
+
+resource "aws_lambda_function" "ToStopEc2" {
+filename = "${local.lambda_zip_location}"
+function_name= "ToStopEc2"
+role = "${aws_iam_role.LambdaToStopAndStartEc2_role.arn}"
+handler = "stopec2.lambda_handler"
+runtime= "python3.7"
 }
